09e7f8855966336c4deac853e2a09b76d275706b
Dzreke-Poku Nathan <dzrekenathan2002@gmail.com>
2024-01-07 00:48:29 +0000

    Update

--- src/main/java/com/example/integratingdata/Integrating/Data/IntegratingDataApplication.java
+++ src/main/java/com/example/integratingdata/Integrating/Data/IntegratingDataApplication.java
@@ -2,12 +2,24 @@ package com.example.integratingdata.Integrating.Data;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ImportResource;
+
+import java.io.IOException;
+
 
 @SpringBootApplication
+@ImportResource("/integration/integration.xml")
 public class IntegratingDataApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IntegratingDataApplication.class, args);
+	public static void main(String[] args) throws IOException {
+		ConfigurableApplicationContext ctx = new
+				SpringApplication(IntegratingDataApplication.class).run(args);
+		System.out.println("Hit Enter to terminate");
+		System.in.read();
+		ctx.close();
 	}
 
 }
