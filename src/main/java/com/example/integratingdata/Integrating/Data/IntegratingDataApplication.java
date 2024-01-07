4035ca05c07a51b408acf51954175656b3f1d79f
Dzreke-Poku Nathan <dzrekenathan2002@gmail.com>
2024-01-07 00:34:25 +0000

    Initial Commit

---
+++ src/main/java/com/example/integratingdata/Integrating/Data/IntegratingDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.integratingdata.Integrating.Data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegratingDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegratingDataApplication.class, args);
+	}
+
+}
